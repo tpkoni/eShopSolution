1146fddd1389188d1886307dc1a05d5246c23290
Phu <phutv1010@gmail.com>
2021-01-14 17:24:21 +0700

    fix error

--- eShopSolution.Data/Entities/Category.cs
+++ eShopSolution.Data/Entities/Category.cs
@@ -1,4 +1,4 @@
-﻿using eShopSolution.Data.Entities.Enums;
+﻿using eShopSolution.Data.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
--- eShopSolution.Data/Enums/Status.cs
+++ eShopSolution.Data/Enums/Status.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace eShopSolution.Data.Entities.Enums
+namespace eShopSolution.Data.Enums
 {
     public enum Status
     {
